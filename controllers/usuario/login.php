8628a1293b2cad6c3b3c84aaee48869135a9ab86
koinu <daniel_ortizpary@hotmail.com>
2022-06-22 22:49:28 -0500

    mejoras

--- controllers/usuario/login.php
+++ controllers/usuario/login.php
@@ -9,6 +9,8 @@ try {
         $contrasena = $_POST['contrasena'];
         echo json_encode(login($correo,$contrasena), JSON_PRETTY_PRINT);
     }else {
+        echo json_encode($_POST['correo']);
+        echo json_encode($_POST['contrasena']);
         echo json_encode('datos incorrectos');
     }
 
